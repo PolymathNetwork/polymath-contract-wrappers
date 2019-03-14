203666da72e4cdbb6fbee887d928da464f831e4b
Shuffledex <shuffledex@gmail.com>
2019-03-14 11:09:00 -0300

    completed security token interfaces

--- src/contract_wrappers/detailed_erc20_wrapper.ts
+++ src/contract_wrappers/detailed_erc20_wrapper.ts
@@ -1,4 +1,10 @@
-import { DetailedERC20Contract, DetailedERC20EventArgs, DetailedERC20Events } from '@polymathnetwork/abi-wrappers';
+import {
+  DetailedERC20Contract,
+  DetailedERC20EventArgs,
+  DetailedERC20Events,
+  DetailedERC20ApprovalEventArgs,
+  DetailedERC20TransferEventArgs,
+} from '@polymathnetwork/abi-wrappers';
 import { PolymathRegistryWrapper } from './polymath_registry_wrapper';
 import { DetailedERC20 } from '@polymathnetwork/contract-artifacts';
 import { Web3Wrapper } from '@0x/web3-wrapper';
@@ -10,15 +16,44 @@ import {
   ITxParams,
   IGetLogsAsyncParams,
   ISubscribeAsyncParams,
+  EventCallback,
 } from '../types';
 import { assert } from '../utils/assert';
 import { schemas } from '@0x/json-schemas';
+
+interface IApprovalSubscribeAsyncParams extends ISubscribeAsyncParams {
+  eventName: DetailedERC20Events.Approval,
+  callback: EventCallback<DetailedERC20ApprovalEventArgs>,
+}
+
+interface IGetApprovalLogsAsyncParams extends IGetLogsAsyncParams {
+  eventName: DetailedERC20Events.Approval,
+}
+
+interface ITransferSubscribeAsyncParams extends ISubscribeAsyncParams {
+  eventName: DetailedERC20Events.Transfer,
+  callback: EventCallback<DetailedERC20TransferEventArgs>,
+}
+
+interface IGetTransferLogsAsyncParams extends IGetLogsAsyncParams {
+  eventName: DetailedERC20Events.Transfer,
+}
+
+interface IDetailedERC20SubscribeAsyncParams {
+  (params: IApprovalSubscribeAsyncParams): Promise<string>,
+  (params: ITransferSubscribeAsyncParams): Promise<string>,
+}
+
+interface IGetDetailedERC20LogsAsyncParams {
+  (params: IGetApprovalLogsAsyncParams): Promise<Array<LogWithDecodedArgs<DetailedERC20ApprovalEventArgs>>>,
+  (params: IGetTransferLogsAsyncParams): Promise<Array<LogWithDecodedArgs<DetailedERC20TransferEventArgs>>>,
+}
 
 /**
  * @param spender The address which will spend the funds
  * @param value The amount of tokens to be spent
  */
-export interface IApproveParams extends ITxParams {
+interface IApproveParams extends ITxParams {
     spender: string;
     value: BigNumber;
 }
@@ -28,7 +63,7 @@ export interface IApproveParams extends ITxParams {
  * @param to The address who will receive the funds
  * @param value The amount of tokens to be spent
  */
-export interface ITransferFromParams extends ITxParams {
+interface ITransferFromParams extends ITxParams {
     from: string;
     to: string;
     value: BigNumber;
@@ -37,7 +72,7 @@ export interface ITransferFromParams extends ITxParams {
 /**
  * @param owner The address to query the the balance of
  */
-export interface IGetBalanceOfParams {
+interface IGetBalanceOfParams {
     owner?: string;
 }
 
@@ -45,7 +80,7 @@ export interface IGetBalanceOfParams {
  * @param to The address who will receive the funds
  * @param value The amount of tokens to be spent
  */
-export interface ITransferParams extends ITxParams {
+interface ITransferParams extends ITxParams {
     to: string;
     value: BigNumber;
 }
@@ -54,7 +89,7 @@ export interface ITransferParams extends ITxParams {
  * @param owner address The address which owns the tokens
  * @param spender address The address which will spend the tokens
  */
-export interface IAllowanceParams {
+interface IAllowanceParams {
     owner: string;
     spender: string;
 }
@@ -182,8 +217,8 @@ export class DetailedERC20Wrapper extends ContractWrapper {
    * Subscribe to an event type emitted by the contract.
    * @return Subscription token used later to unsubscribe
    */
-  public subscribeAsync = async <ArgsType extends DetailedERC20EventArgs>(
-    params: ISubscribeAsyncParams<DetailedERC20Events, ArgsType>
+  public subscribeAsync: IDetailedERC20SubscribeAsyncParams = async <ArgsType extends DetailedERC20EventArgs>(
+    params: ISubscribeAsyncParams
   ): Promise<string> => {
     assert.doesBelongToStringEnum('eventName', params.eventName, DetailedERC20Events);
     assert.doesConformToSchema('indexFilterValues', params.indexFilterValues, schemas.indexFilterValuesSchema);
@@ -220,8 +255,8 @@ export class DetailedERC20Wrapper extends ContractWrapper {
    * Gets historical logs without creating a subscription
    * @return Array of logs that match the parameters
    */
-  public getLogsAsync = async <ArgsType extends DetailedERC20EventArgs>(
-    params: IGetLogsAsyncParams<DetailedERC20Events>
+  public getLogsAsync: IGetDetailedERC20LogsAsyncParams = async <ArgsType extends DetailedERC20EventArgs>(
+    params: IGetLogsAsyncParams
   ): Promise<Array<LogWithDecodedArgs<ArgsType>>> => {
     assert.doesBelongToStringEnum('eventName', params.eventName, DetailedERC20Events);
     assert.doesConformToSchema('blockRange', params.blockRange, schemas.blockRangeSchema);
