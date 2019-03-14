7ecb4c64163cd15621e6080ce92c133f16a39d9e
Shuffledex <shuffledex@gmail.com>
2019-03-14 16:05:30 -0300

    all methods implemented

--- src/contract_wrappers/security_token_registry_wrapper.ts
+++ src/contract_wrappers/security_token_registry_wrapper.ts
@@ -207,6 +207,132 @@ interface IGenerateSecurityTokenParams extends ITxParams {
 }
 
 /**
+ * @param polymathRegistry is the address of the Polymath Registry
+ * @param STFactory is the address of the Proxy contract for Security Tokens
+ * @param stLaunchFee is the fee in POLY required to launch a token
+ * @param tickerRegFee is the fee in POLY required to register a ticker
+ * @param polyToken is the address of the POLY ERC20 token
+ * @param owner is the owner of the STR
+ */
+interface IInitializeParams extends ITxParams {
+  polymathRegistry: string,
+  STFactory: string,
+  stLaunchFee: BigNumber,
+  tickerRegFee: BigNumber,
+  polyToken: string,
+  owner: string,
+}
+
+/**
+ * @param owner is the owner of the token
+ * @param ticker is the token ticker
+ * @param tokenName is the name of the token
+ * @param registrationDate is the date at which ticker is registered
+ * @param expiryDate is the expiry date for the ticker
+ * @param status is the token deployment status
+ */
+interface IModifyTickerParams extends ITxParams {
+  owner: string,
+  ticker: string,
+  tokenName: string,
+  registrationDate: BigNumber,
+  expiryDate: BigNumber,
+  status: boolean,
+}
+
+/**
+ * @param ticker is the token ticker
+ */
+interface IRemoveTickerParams extends ITxParams {
+  ticker: string,
+}
+
+/**
+ * @param newExpiry is the new expiry for newly generated tickers
+ */
+interface IChangeExpiryLimitParams extends ITxParams {
+  newExpiry: BigNumber,
+}
+
+/**
+ * @param name is the name of the token
+ * @param ticker is the ticker symbol of the security token
+ * @param owner is the owner of the token
+ * @param securityToken is the address of the securityToken
+ * @param tokenDetails is the off-chain details of the token
+ * @param deployedAt is the timestamp at which the security token is deployed
+ */
+interface IModifySecurityTokenParams extends ITxParams {
+  name: string,
+  ticker: string,
+  owner: string,
+  securityToken: string,
+  tokenDetails: string,
+  deployedAt: BigNumber,
+}
+
+/**
+  * @param securityToken is the address of the security token
+  */
+interface IIsSecurityTokenParams extends ITxParams {
+  securityToken: string,
+}
+
+/**
+  * @param newOwner The address to transfer ownership to.
+  */
+interface ITransferOwnershipParams extends ITxParams {
+  newOwner: string,
+}
+
+interface IPauseParams extends ITxParams {
+}
+
+interface IUnpauseParams extends ITxParams {
+}
+
+/**
+  * @param tickerRegFee is the registration fee in POLY tokens (base 18 decimals)
+  */
+interface IChangeTickerRegistrationFeeParams extends ITxParams {
+  tickerRegFee: BigNumber,
+}
+
+/**
+  * @param _stLaunchFee is the registration fee in POLY tokens (base 18 decimals)
+  */
+interface IChangeSecurityLaunchFeeParams extends ITxParams {
+  stLaunchFee: BigNumber,
+}
+
+/**
+  * @param tokenContract is the address of the token contract
+  */
+interface IReclaimERC20Params extends ITxParams {
+  tokenContract: string
+}
+
+/**
+  * @param STFactoryAddress is the address of the proxy.
+  * @param major Major version of the proxy.
+  * @param minor Minor version of the proxy.
+  * @param patch Patch version of the proxy
+  */
+interface ISetProtocolVersionParams extends ITxParams {
+  STFactoryAddress: string,
+  major: number|BigNumber,
+  minor: number|BigNumber,
+  patch: number|BigNumber,
+}
+
+/**
+ * @param newAddress is the address of the polytoken.
+ */
+interface IUpdatePolyTokenAddressParams extends ITxParams {
+  newAddress: string,
+}
+
+/**
  * This class includes the functionality related to interacting with the SecurityTokenRegistry contract.
  */
 export class SecurityTokenRegistryWrapper extends ContractWrapper {
@@ -390,6 +516,213 @@ export class SecurityTokenRegistryWrapper extends ContractWrapper {
     const result = await this.getTickerDetails(params);
     const tokenDeployed = result[4];
     return tokenDeployed;
+  }
+
+  /**
+   * Initializes instance of STR
+   */
+  public initialize = async (params: IInitializeParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).initialize.sendTransactionAsync(
+        params.polymathRegistry,
+        params.STFactory,
+        params.stLaunchFee,
+        params.tickerRegFee,
+        params.polyToken,
+        params.owner,
+      );
+    }
+  }
+
+  /**
+   * Modifies the ticker details. Only Polymath has the ability to do so.
+   */
+  public modifyTicker = async (params: IModifyTickerParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).modifyTicker.sendTransactionAsync(
+        params.owner,
+        params.ticker,
+        params.tokenName,
+        params.registrationDate,
+        params.expiryDate,
+        params.status,
+      );
+    }
+  }
+
+  /**
+   * Removes the ticker details, associated ownership & security token mapping
+   */
+  public removeTicker = async (params: IRemoveTickerParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).removeTicker.sendTransactionAsync(
+        params.ticker,
+      );
+    }
+  }
+
+  /**
+   * Changes the expiry time for the token ticker. Only available to Polymath.
+   */
+  public changeExpiryLimit = async (params: IChangeExpiryLimitParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).changeExpiryLimit.sendTransactionAsync(
+        params.newExpiry,
+      );
+    }
+  }
+
+  /**
+   * Adds a new custom Security Token and saves it to the registry. (Token should follow the ISecurityToken interface)
+   */
+  public modifySecurityToken = async (params: IModifySecurityTokenParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).modifySecurityToken.sendTransactionAsync(
+        params.owner,
+        params.ticker,
+        params.owner,
+        params.securityToken,
+        params.tokenDetails,
+        params.deployedAt,
+      );
+    }
+  }
+
+  /**
+   * Checks that Security Token is registered
+   */
+  public isSecurityToken = async (params: IIsSecurityTokenParams): Promise<boolean> => {
+    return await (await this.securityTokenRegistryContract).isSecurityToken.callAsync(
+      params.securityToken,
+    );
+  }
+
+  /**
+   * Allows the current owner to transfer control of the contract to a newOwner.
+   */
+  public transferOwnership = async (params: ITransferOwnershipParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).transferOwnership.sendTransactionAsync(
+        params.newOwner,
+      );
+    }
+  }
+
+  /**
+   * Called by the owner to pause, triggers stopped state
+   */
+  public pause = async (params: IPauseParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).pause.sendTransactionAsync(
+        params.txData,
+        params.safetyFactor,
+      );
+    }
+  }
+
+  /**
+   * Called by the owner to unpause, returns to normal state
+   */
+  public unpause = async (params: IUnpauseParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).unpause.sendTransactionAsync(
+        params.txData,
+        params.safetyFactor,
+      );
+    }
+  }
+
+  /**
+   * Sets the ticker registration fee in POLY tokens. Only Polymath.
+   */
+  public changeTickerRegistrationFee = async (params: IChangeTickerRegistrationFeeParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).changeTickerRegistrationFee.sendTransactionAsync(
+        params.tickerRegFee,
+      );
+    }
+  }
+
+  /**
+   * Sets the ticker registration fee in POLY tokens. Only Polymath.
+   */
+  public changeSecurityLaunchFee = async (params: IChangeSecurityLaunchFeeParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).changeSecurityLaunchFee.sendTransactionAsync(
+        params.stLaunchFee,
+      );
+    }
+  }
+
+  /**
+   * Reclaims all ERC20Basic compatible tokens
+   */
+  public reclaimERC20 = async (params: IReclaimERC20Params) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).reclaimERC20.sendTransactionAsync(
+        params.tokenContract,
+      );
+    }
+  }
+
+  /**
+   * Changes the protocol version and the SecurityToken contract
+   */
+  public setProtocolVersion = async (params: ISetProtocolVersionParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).setProtocolVersion.sendTransactionAsync(
+        params.STFactoryAddress,
+        params.major,
+        params.minor,
+        params.patch,
+      );
+    }
+  }
+
+  /**
+   * Returns the current STFactory Address
+   */
+  public getSTFactoryAddress = async (): Promise<string> => {
+    return await (await this.securityTokenRegistryContract).getSTFactoryAddress.callAsync();
+  }
+
+  /**
+   * Gets Protocol version
+   */
+  public getProtocolVersion = async (): Promise<BigNumber[]> => {
+    return await (await this.securityTokenRegistryContract).getProtocolVersion.callAsync();
+  }
+
+  /**
+   * Changes the PolyToken address. Only Polymath.
+   */
+  public updatePolyTokenAddress = async (params: IUpdatePolyTokenAddressParams) => {
+    return async () => {
+      return (await this.securityTokenRegistryContract).updatePolyTokenAddress.sendTransactionAsync(
+        params.newAddress,
+      );
+    }
+  }
+
+  /**
+   * Gets the expiry limit
+   */
+  public getExpiryLimit = async (): Promise<BigNumber> => {
+    return await (await this.securityTokenRegistryContract).getExpiryLimit.callAsync();
+  }
+
+  /**
+   * Check whether the registry is paused or not
+   */
+  public isPaused = async (): Promise<boolean> => {
+    return await (await this.securityTokenRegistryContract).isPaused.callAsync();
+  }
+
+  /**
+   * Gets the owner of the contract
+   */
+  public owner = async (): Promise<string> => {
+    return await (await this.securityTokenRegistryContract).owner.callAsync();
   }
 
   /**
