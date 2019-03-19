9c372a6b2739119916c4f15cbd3088e9035f9acf
Victor <victor.g.vicente@gmail.com>
2019-03-19 10:02:58 -0300

    GTM added to ModuleFactory

--- src/factories/ModuleWrapperFactory.ts
+++ src/factories/ModuleWrapperFactory.ts
@@ -1,22 +1,20 @@
 import { Web3Wrapper } from "@0x/web3-wrapper";
-import { CappedSTOWrapper } from "contract_wrappers/capped_sto_wrapper";
-import { USDTieredSTOWrapper } from "contract_wrappers/usd_tiered_sto_wrapper";
+import { CappedSTOWrapper } from "../contract_wrappers/capped_sto_wrapper";
+import { USDTieredSTOWrapper } from "../contract_wrappers/usd_tiered_sto_wrapper";
+import { GeneralTransferManagerWrapper } from "../contract_wrappers/general_transfer_manager_wrapper";
 
 interface IGetModuleParams {
   address: string, 
-  type: 1 | 2 | 3 | 4 | 5, 
   name: string
 }
 
 interface IGetCappedSTO extends IGetModuleParams {
   address: string, 
-  type: 3, 
   name: 'CappedSTO'
 }
 
 interface IGetUSDTieredSTO extends IGetModuleParams {
   address: string, 
-  type: 3, 
   name: 'USDTieredSTO'
 }
 
@@ -37,27 +35,18 @@ export class ModuleWrapperFactory {
   }
 
   public getModuleInstance: IGetModuleInstance = async (params: IGetModuleParams): Promise<any> => {
-    switch (params.type) {
-      case 1:
-        //Permission modules
-        break;
-      case 2:
-        //Transfer modules
-        break;
-      case 3:
-        switch (params.name) {
-          case 'CappedSTO':
-            return new CappedSTOWrapper(this._web3Wrapper, params.address);
-          case 'USDTieredSTO':
-            return new USDTieredSTOWrapper(this._web3Wrapper, params.address);
-        }
-      case 4:
-        //Checkpoint modules
-        break;
-      case 5:
-        //Burn modules
-        break;
+    switch (params.name) {
+      // Permission
+      // TMs
+      case 'GeneralTransferManager':
+        return new GeneralTransferManagerWrapper(this._web3Wrapper, params.address);
+      // STOs
+      case 'CappedSTO':
+        return new CappedSTOWrapper(this._web3Wrapper, params.address);
+      case 'USDTieredSTO':
+        return new USDTieredSTOWrapper(this._web3Wrapper, params.address);
+      // Checkpoint
+      // Burn
     }
-    
   }
 }
