185f7a704e304506093b55208a99aad70cd74e9d
Victor <victor.g.vicente@gmail.com>
2019-04-08 13:13:24 -0300

    Airbnb codestyle

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parserOptions: {
+        "project": "./tsconfig.json"
+    },
     extends: [
-        'airbnb',
+        'airbnb-base',
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
