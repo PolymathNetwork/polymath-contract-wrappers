7759b3543ed80fe0ba4c3288cd5bb339f79fd50c
Victor Wiebe <victor.wiebe@polymath.network>
2019-05-08 08:33:02 -0800

    Two new tests for usdtiered sto

--- src/contract_wrappers/modules/sto/__tests__/usd_tiered_sto_wrapper.test.ts
+++ src/contract_wrappers/modules/sto/__tests__/usd_tiered_sto_wrapper.test.ts
@@ -1,5 +1,5 @@
 // USDTieredSTOWrapper test
-import { mock, instance, reset, when, verify } from 'ts-mockito';
+import { mock, instance, reset, when, verify, objectContaining } from 'ts-mockito';
 import { BigNumber } from '@0x/utils';
 import { Web3Wrapper } from '@0x/web3-wrapper';
 import { USDTieredSTOContract, SecurityTokenContract, PolyTokenEvents } from '@polymathnetwork/abi-wrappers';
@@ -7,6 +7,7 @@ import { getMockedPolyResponse, MockedCallMethod, MockedSendMethod } from '../..
 import USDTieredSTOWrapper from '../usd_tiered_sto_wrapper';
 import ContractFactory from '../../../../factories/contractFactory';
 import STOWrapper from '../sto_wrapper';
+import { numberToBigNumber } from '../../../../utils/convert';
 
 describe('USDTieredSTOWrapper', () => {
   // Capped STO Wrapper is used as contract target here as STOWrapper is abstract
@@ -35,6 +36,51 @@ describe('USDTieredSTOWrapper', () => {
   describe('Types', () => {
     test('should extend STOWrapper', async () => {
       expect(target instanceof STOWrapper).toBe(true);
+    });
+  });
+
+  describe('InvestorsList', () => {
+    test('should get address in investors list', async () => {
+      // Address expected
+      const expectedResult = '0x1111111111111111111111111111111111111111';
+      const params = { investorIndex: 1 };
+      // Mocked method
+      const mockedMethod = mock(MockedCallMethod);
+      // Stub the method
+      when(mockedContract.investorsList).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(mockedMethod.callAsync(objectContaining(numberToBigNumber(params.investorIndex)))).thenResolve(
+        expectedResult,
+      );
+
+      // Real call
+      const result = await target.investorsList(params);
+      // Result expectation
+      expect(result).toEqual(expectedResult);
+      // Verifications
+      verify(mockedContract.investorsList).once();
+      verify(mockedMethod.callAsync(objectContaining(numberToBigNumber(params.investorIndex)))).once();
+    });
+  });
+
+  describe('AllowBeneficialInvestments', () => {
+    test('should get boolean of allowBeneficialInvestments', async () => {
+      // Address expected
+      const expectedResult = true;
+      // Mocked method
+      const mockedMethod = mock(MockedCallMethod);
+      // Stub the method
+      when(mockedContract.allowBeneficialInvestments).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(mockedMethod.callAsync()).thenResolve(expectedResult);
+
+      // Real call
+      const result = await target.allowBeneficialInvestments();
+      // Result expectation
+      expect(result).toBe(expectedResult);
+      // Verifications
+      verify(mockedContract.allowBeneficialInvestments).once();
+      verify(mockedMethod.callAsync()).once();
     });
   });
 
