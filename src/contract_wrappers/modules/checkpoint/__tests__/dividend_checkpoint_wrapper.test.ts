27b8b91555ead855b100554c0b63f56c0358fa19
Victor Wiebe <victor.wiebe@polymath.network>
2019-05-14 08:52:35 -0800

    Dividend Checkpoint tests

--- src/contract_wrappers/modules/checkpoint/__tests__/dividend_checkpoint_wrapper.test.ts
+++ src/contract_wrappers/modules/checkpoint/__tests__/dividend_checkpoint_wrapper.test.ts
@@ -1,5 +1,5 @@
 // DividendCheckpointWrapper test
-import { mock, instance, reset, when, verify } from 'ts-mockito';
+import { mock, instance, reset, when, verify, objectContaining } from 'ts-mockito';
 import { BigNumber } from '@0x/utils';
 import { Web3Wrapper } from '@0x/web3-wrapper';
 import { ERC20DividendCheckpointContract, SecurityTokenContract } from '@polymathnetwork/abi-wrappers';
@@ -7,6 +7,7 @@ import { getMockedPolyResponse, MockedCallMethod, MockedSendMethod } from '../..
 import ContractWrapper from '../../../contract_wrapper';
 import ERC20DividendCheckpointWrapper from '../erc20_dividend_checkpoint_wrapper';
 import ContractFactory from '../../../../factories/contractFactory';
+import { dateToBigNumber, stringToBytes32 } from '../../../../utils/convert';
 
 describe('DividendCheckpointWrapper', () => {
   // ERC20 Dividend Wrapper is used as contract target here as DividendCheckpoint is abstract
@@ -204,6 +205,95 @@ describe('DividendCheckpointWrapper', () => {
     });
   });
 
+  describe('Dividends', () => {
+    test('should get dividend info', async () => {
+      const dividendIndex = 2;
+      const expectedResult = [
+        new BigNumber(1),
+        new BigNumber(1),
+        new BigNumber(1),
+        new BigNumber(1),
+        new BigNumber(1),
+        new BigNumber(1),
+        new BigNumber(1),
+        false,
+        new BigNumber(1),
+        new BigNumber(1),
+        stringToBytes32('name'),
+      ];
+      // Mocked method
+      const mockedMethod = mock(MockedCallMethod);
+      // Stub the method
+      when(mockedContract.dividends).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(mockedMethod.callAsync(objectContaining(new BigNumber(dividendIndex)))).thenResolve(expectedResult);
+
+      // Real call
+      const result = await target.dividends({ dividendIndex });
+
+      // Result expectation
+      expect(new BigNumber(1)).toEqual(expectedResult[0]);
+      expect(dateToBigNumber(result.created)).toEqual(expectedResult[1]);
+      expect(dateToBigNumber(result.maturity)).toEqual(expectedResult[2]);
+      expect(dateToBigNumber(result.expiry)).toEqual(expectedResult[3]);
+      expect(result.amount).toBe(expectedResult[4]);
+      expect(result.claimedAmount).toBe(expectedResult[5]);
+      expect(result.totalSupply).toBe(expectedResult[6]);
+      expect(result.reclaimed).toBe(expectedResult[7]);
+      expect(result.totalWithheld).toBe(expectedResult[8]);
+      expect(result.totalWithheldWithdrawn).toBe(expectedResult[9]);
+      expect(result.name).toBe(expectedResult[10]);
+
+      // Verifications
+      verify(mockedContract.dividends).once();
+      verify(mockedMethod.callAsync(objectContaining(new BigNumber(dividendIndex)))).once();
+    });
+  });
+
+  describe('Excluded', () => {
+    test('should get excluded address', async () => {
+      const dividendIndex = 2;
+      const expectedResult = '0x1111111111111111111111111111111111111111';
+      // Mocked method
+      const mockedMethod = mock(MockedCallMethod);
+      // Stub the method
+      when(mockedContract.excluded).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(mockedMethod.callAsync(objectContaining(new BigNumber(dividendIndex)))).thenResolve(expectedResult);
+
+      // Real call
+      const result = await target.excluded({ dividendIndex });
+
+      // Result expectation
+      expect(result).toBe(expectedResult);
+      // Verifications
+      verify(mockedContract.excluded).once();
+      verify(mockedMethod.callAsync(objectContaining(new BigNumber(dividendIndex)))).once();
+    });
+  });
+
+  describe('WithholdingTax', () => {
+    test('should get withholdingTax', async () => {
+      const investor = '0x1111111111111111111111111111111111111111';
+      const expectedResult = new BigNumber(1);
+      // Mocked method
+      const mockedMethod = mock(MockedCallMethod);
+      // Stub the method
+      when(mockedContract.withholdingTax).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(mockedMethod.callAsync(investor)).thenResolve(expectedResult);
+
+      // Real call
+      const result = await target.withholdingTax({ investor });
+
+      // Result expectation
+      expect(result).toBe(expectedResult);
+      // Verifications
+      verify(mockedContract.withholdingTax).once();
+      verify(mockedMethod.callAsync(investor)).once();
+    });
+  });
+
   describe('ReclaimERC20', () => {
     test('should call to Reclaim ERC20', async () => {
       // Owner Address expected
@@ -301,6 +391,182 @@ describe('DividendCheckpointWrapper', () => {
       // Verifications
       verify(mockedContract.reclaimETH).once();
       verify(mockedMethod.sendTransactionAsync(mockedParams.txData, mockedParams.safetyFactor)).once();
+      verify(mockedSecurityTokenOwnerMethod.callAsync()).once();
+      verify(mockedSecurityTokenContract.owner).once();
+    });
+  });
+
+  describe('Change Wallet', () => {
+    test('should change wallet', async () => {
+      // Owner Address expected
+      const expectedOwnerResult = '0x5555555555555555555555555555555555555555';
+
+      // Security Token Address expected
+      const expectedSecurityTokenAddress = '0x3333333333333333333333333333333333333333';
+      // Setup get Security Token Address
+      const mockedGetSecurityTokenAddressMethod = mock(MockedCallMethod);
+      when(mockedContract.securityToken).thenReturn(instance(mockedGetSecurityTokenAddressMethod));
+      when(mockedGetSecurityTokenAddressMethod.callAsync()).thenResolve(expectedSecurityTokenAddress);
+      when(mockedContractFactory.getSecurityTokenContract(expectedSecurityTokenAddress)).thenResolve(
+        instance(mockedSecurityTokenContract),
+      );
+      const mockedSecurityTokenOwnerMethod = mock(MockedCallMethod);
+      when(mockedSecurityTokenOwnerMethod.callAsync()).thenResolve(expectedOwnerResult);
+      when(mockedSecurityTokenContract.owner).thenReturn(instance(mockedSecurityTokenOwnerMethod));
+
+      // Mock web3 wrapper owner
+      when(mockedWrapper.getAvailableAddressesAsync()).thenResolve([expectedOwnerResult]);
+
+      const mockedParams = {
+        wallet: '0x4444444444444444444444444444444444444444',
+        txData: {},
+        safetyFactor: 10,
+      };
+      const expectedResult = getMockedPolyResponse();
+      // Mocked method
+      const mockedMethod = mock(MockedSendMethod);
+      // Stub the method
+      when(mockedContract.changeWallet).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(
+        mockedMethod.sendTransactionAsync(mockedParams.wallet, mockedParams.txData, mockedParams.safetyFactor),
+      ).thenResolve(expectedResult);
+
+      // Real call
+      const result = await target.changeWallet(mockedParams);
+
+      // Result expectation
+      expect(result).toBe(expectedResult);
+      // Verifications
+      verify(mockedContract.changeWallet).once();
+      verify(
+        mockedMethod.sendTransactionAsync(mockedParams.wallet, mockedParams.txData, mockedParams.safetyFactor),
+      ).once();
+      verify(mockedSecurityTokenOwnerMethod.callAsync()).once();
+      verify(mockedSecurityTokenContract.owner).once();
+    });
+  });
+
+  describe('GetDefaultExcluded', () => {
+    test('should getDefaultExcluded', async () => {
+      const expectedResult = [
+        '0x1111111111111111111111111111111111111111',
+        '0x2222222222222222222222222222222222222222',
+      ];
+
+      // Mocked method
+      const mockedMethod = mock(MockedCallMethod);
+      // Stub the method
+      when(mockedContract.getDefaultExcluded).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(mockedMethod.callAsync()).thenResolve(expectedResult);
+
+      // Real call
+      const result = await target.getDefaultExcluded();
+
+      // Result expectation
+      expect(result).toBe(expectedResult);
+
+      // Verifications
+      verify(mockedContract.getDefaultExcluded).once();
+      verify(mockedMethod.callAsync()).once();
+    });
+  });
+
+  describe('Create Checkpoint', () => {
+    test('should create checkpoint', async () => {
+      // Owner Address expected
+      const expectedOwnerResult = '0x5555555555555555555555555555555555555555';
+
+      // Security Token Address expected
+      const expectedSecurityTokenAddress = '0x3333333333333333333333333333333333333333';
+      // Setup get Security Token Address
+      const mockedGetSecurityTokenAddressMethod = mock(MockedCallMethod);
+      when(mockedContract.securityToken).thenReturn(instance(mockedGetSecurityTokenAddressMethod));
+      when(mockedGetSecurityTokenAddressMethod.callAsync()).thenResolve(expectedSecurityTokenAddress);
+      when(mockedContractFactory.getSecurityTokenContract(expectedSecurityTokenAddress)).thenResolve(
+        instance(mockedSecurityTokenContract),
+      );
+      const mockedSecurityTokenOwnerMethod = mock(MockedCallMethod);
+      when(mockedSecurityTokenOwnerMethod.callAsync()).thenResolve(expectedOwnerResult);
+      when(mockedSecurityTokenContract.owner).thenReturn(instance(mockedSecurityTokenOwnerMethod));
+
+      // Mock web3 wrapper owner
+      when(mockedWrapper.getAvailableAddressesAsync()).thenResolve([expectedOwnerResult]);
+
+      const mockedParams = {
+        txData: {},
+        safetyFactor: 10,
+      };
+      const expectedResult = getMockedPolyResponse();
+      // Mocked method
+      const mockedMethod = mock(MockedSendMethod);
+      // Stub the method
+      when(mockedContract.createCheckpoint).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(mockedMethod.sendTransactionAsync(mockedParams.txData, mockedParams.safetyFactor)).thenResolve(
+        expectedResult,
+      );
+
+      // Real call
+      const result = await target.createCheckpoint(mockedParams);
+
+      // Result expectation
+      expect(result).toBe(expectedResult);
+      // Verifications
+      verify(mockedContract.createCheckpoint).once();
+      verify(mockedMethod.sendTransactionAsync(mockedParams.txData, mockedParams.safetyFactor)).once();
+      verify(mockedSecurityTokenOwnerMethod.callAsync()).once();
+      verify(mockedSecurityTokenContract.owner).once();
+    });
+  });
+
+  describe('Set Default Excluded', () => {
+    test('should setDefaultExcluded', async () => {
+      // Owner Address expected
+      const expectedOwnerResult = '0x5555555555555555555555555555555555555555';
+
+      // Security Token Address expected
+      const expectedSecurityTokenAddress = '0x3333333333333333333333333333333333333333';
+      // Setup get Security Token Address
+      const mockedGetSecurityTokenAddressMethod = mock(MockedCallMethod);
+      when(mockedContract.securityToken).thenReturn(instance(mockedGetSecurityTokenAddressMethod));
+      when(mockedGetSecurityTokenAddressMethod.callAsync()).thenResolve(expectedSecurityTokenAddress);
+      when(mockedContractFactory.getSecurityTokenContract(expectedSecurityTokenAddress)).thenResolve(
+        instance(mockedSecurityTokenContract),
+      );
+      const mockedSecurityTokenOwnerMethod = mock(MockedCallMethod);
+      when(mockedSecurityTokenOwnerMethod.callAsync()).thenResolve(expectedOwnerResult);
+      when(mockedSecurityTokenContract.owner).thenReturn(instance(mockedSecurityTokenOwnerMethod));
+
+      // Mock web3 wrapper owner
+      when(mockedWrapper.getAvailableAddressesAsync()).thenResolve([expectedOwnerResult]);
+
+      const mockedParams = {
+        excluded: ['0x1111111111111111111111111111111111111111', '0x2222222222222222222222222222222222222222'],
+        txData: {},
+        safetyFactor: 10,
+      };
+      const expectedResult = getMockedPolyResponse();
+      // Mocked method
+      const mockedMethod = mock(MockedSendMethod);
+      // Stub the method
+      when(mockedContract.setDefaultExcluded).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(
+        mockedMethod.sendTransactionAsync(mockedParams.excluded, mockedParams.txData, mockedParams.safetyFactor),
+      ).thenResolve(expectedResult);
+
+      // Real call
+      const result = await target.setDefaultExcluded(mockedParams);
+
+      // Result expectation
+      expect(result).toBe(expectedResult);
+      // Verifications
+      verify(mockedContract.setDefaultExcluded).once();
+      verify(
+        mockedMethod.sendTransactionAsync(mockedParams.excluded, mockedParams.txData, mockedParams.safetyFactor),
+      ).once();
       verify(mockedSecurityTokenOwnerMethod.callAsync()).once();
       verify(mockedSecurityTokenContract.owner).once();
     });
