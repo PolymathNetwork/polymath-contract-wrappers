3db4b96633cc1a4db9753aa30c0e7023301f5bfa
Victor Wiebe <victor.wiebe@polymath.network>
2019-05-16 13:37:27 -0800

    Complex dividend tests

--- src/contract_wrappers/modules/checkpoint/__tests__/erc20_dividend_checkpoint_wrapper.test.ts
+++ src/contract_wrappers/modules/checkpoint/__tests__/erc20_dividend_checkpoint_wrapper.test.ts
@@ -7,6 +7,7 @@ import { getMockedPolyResponse, MockedCallMethod, MockedSendMethod } from '../..
 import ERC20DividendCheckpointWrapper from '../erc20_dividend_checkpoint_wrapper';
 import ContractFactory from '../../../../factories/contractFactory';
 import DividendCheckpointWrapper from '../dividend_checkpoint_wrapper';
+import {dateToBigNumber, stringToBytes32} from '../../../../utils/convert';
 
 describe('ERC20DividendCheckpointWrapper', () => {
   // ERC20 Dividend Wrapper is used as contract target here as DividendCheckpoint is abstract
@@ -39,6 +40,115 @@ describe('ERC20DividendCheckpointWrapper', () => {
   describe('Types', () => {
     test('should extend DividendCheckpoint', async () => {
       expect(target instanceof DividendCheckpointWrapper).toBe(true);
+    });
+  });
+
+  describe('DividendTokens', () => {
+    test('should get dividendTokens', async () => {
+      // Address expected
+      const expectedResult = '0x2222222222222222222222222222222222222222';
+      const mockedParams = {
+        dividendIndex: 2,
+      };
+      // Mocked method
+      const mockedMethod = mock(MockedCallMethod);
+      // Stub the method
+      when(mockedContract.dividendTokens).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(mockedMethod.callAsync(objectContaining(new BigNumber(mockedParams.dividendIndex)))).thenResolve(expectedResult);
+
+      // Real call
+      const result = await target.dividendTokens(mockedParams);
+      // Result expectation
+      expect(result).toBe(expectedResult);
+      // Verifications
+      verify(mockedContract.dividendTokens).once();
+      verify(mockedMethod.callAsync(objectContaining(new BigNumber(mockedParams.dividendIndex)))).once();
+    });
+  });
+
+
+  describe('Create Dividend', () => {
+    test('should createDividend', async () => {
+      // Owner Address expected
+      const expectedOwnerResult = '0x5555555555555555555555555555555555555555';
+      const checkpointId = 2;
+
+      // Security Token Address expected
+      const expectedSecurityTokenAddress = '0x3333333333333333333333333333333333333333';
+      // Setup get Security Token Address
+      const mockedGetSecurityTokenAddressMethod = mock(MockedCallMethod);
+      when(mockedContract.securityToken).thenReturn(instance(mockedGetSecurityTokenAddressMethod));
+      when(mockedGetSecurityTokenAddressMethod.callAsync()).thenResolve(expectedSecurityTokenAddress);
+      when(mockedContractFactory.getSecurityTokenContract(expectedSecurityTokenAddress)).thenResolve(
+          instance(mockedSecurityTokenContract),
+      );
+      const mockedSecurityTokenOwnerMethod = mock(MockedCallMethod);
+      when(mockedSecurityTokenOwnerMethod.callAsync()).thenResolve(expectedOwnerResult);
+      when(mockedSecurityTokenContract.owner).thenReturn(instance(mockedSecurityTokenOwnerMethod));
+
+      // Mock web3 wrapper owner
+      when(mockedWrapper.getAvailableAddressesAsync()).thenResolve([expectedOwnerResult]);
+
+      // Mock security token currentCheckpointId
+      const expectedCurrentCheckpointResult = new BigNumber(checkpointId);
+      // Mocked method
+      const mockedCurrentCheckpointMethod = mock(MockedCallMethod);
+      // Stub the method
+      when(mockedSecurityTokenContract.currentCheckpointId).thenReturn(instance(mockedCurrentCheckpointMethod));
+      // Stub the request
+      when(mockedCurrentCheckpointMethod.callAsync()).thenResolve(expectedCurrentCheckpointResult);
+
+      const mockedParams = {
+        maturity: new Date(2030, 1),
+        expiry: new Date(2035, 1),
+        amount: new BigNumber(10),
+        token: '0x3333333333333333333333333333333333333333',
+        name: 'Name',
+        txData: {},
+        safetyFactor: 10,
+      };
+      const expectedResult = getMockedPolyResponse();
+      // Mocked method
+      const mockedMethod = mock(MockedSendMethod);
+      // Stub the method
+      when(mockedContract.createDividend).thenReturn(instance(mockedMethod));
+      // Stub the request
+      when(
+          mockedMethod.sendTransactionAsync(
+              objectContaining(dateToBigNumber(mockedParams.maturity)),
+              objectContaining(dateToBigNumber(mockedParams.expiry)),
+              mockedParams.token,
+              objectContaining(new BigNumber(mockedParams.amount)),
+              objectContaining(stringToBytes32(mockedParams.name)),
+              mockedParams.txData,
+              mockedParams.safetyFactor,
+          ),
+
+      ).thenResolve(expectedResult);
+
+      // Real call
+      const result = await target.createDividend(mockedParams);
+
+      // Result expectation
+      expect(result).toBe(expectedResult);
+      // Verifications
+      verify(mockedContract.createDividend).once();
+      verify(
+          mockedMethod.sendTransactionAsync(
+              objectContaining(dateToBigNumber(mockedParams.maturity)),
+              objectContaining(dateToBigNumber(mockedParams.expiry)),
+              mockedParams.token,
+              objectContaining(new BigNumber(mockedParams.amount)),
+              objectContaining(stringToBytes32(mockedParams.name)),
+              mockedParams.txData,
+              mockedParams.safetyFactor,
+          ),
+      ).once();
+      verify(mockedSecurityTokenOwnerMethod.callAsync()).once();
+      verify(mockedSecurityTokenContract.owner).once();
+      verify(mockedCurrentCheckpointMethod.callAsync()).never();
+      verify(mockedSecurityTokenContract.currentCheckpointId).never();
     });
   });
 
