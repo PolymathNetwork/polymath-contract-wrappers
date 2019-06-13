2461e9338c0c5106c66a0367b097d44ed5ac6ac0
Shuffledex <shuffledex@gmail.com>
2019-06-13 12:56:23 -0300

    mint tokens

---
+++ examples/mintTokenToInvestor.ts
@@ -0,0 +1,54 @@
+import { BigNumber } from '@0x/utils';
+import { RedundantSubprovider, RPCSubprovider, Web3ProviderEngine } from '@0x/subproviders';
+import { SecurityTokenEvents } from '@polymathnetwork/abi-wrappers';
+import { ApiConstructorParams, PolymathAPI } from '../src/PolymathAPI';
+
+// This file acts as a valid sandbox.ts file in root directory for adding a cappedSTO module on an unlocked node (like ganache)
+
+window.addEventListener('load', async () => {
+  // Setup the redundant provider
+  const providerEngine = new Web3ProviderEngine();
+  providerEngine.addProvider(new RedundantSubprovider([new RPCSubprovider('http://127.0.0.1:8545')]));
+  providerEngine.start();
+  const params: ApiConstructorParams = {
+    provider: providerEngine,
+    polymathRegistryAddress: '<Deployed Polymath Registry Address>',
+  };
+
+  // Instantiate the API
+  const polymathAPI = new PolymathAPI(params);
+
+  const ticker = 'TEST';
+  const tokenAddress = await polymathAPI.securityTokenRegistry.getSecurityTokenAddress(ticker);
+  const TEST = await polymathAPI.tokenFactory.getSecurityTokenInstanceFromAddress(tokenAddress);
+  const investorAddress = '<investor address>'.toLowerCase();
+
+  const listInvestors = await TEST.getInvestors();
+  const found = listInvestors.find(function(addr) {
+    return addr == investorAddress;
+  });
+
+  if (found) {
+    await TEST.mint({
+      investor: investorAddress,
+      value: new BigNumber(100),
+      txData: {
+        from: '<sto owner>'.toLowerCase(),
+      },
+    });
+  } else {
+    console.log('Please make sure beneficiary address has been whitelisted');
+  }
+
+  await TEST.subscribeAsync({
+    eventName: SecurityTokenEvents.Minted,
+    indexFilterValues: {},
+    callback: async (error, log) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Minted', log);
+      }
+    },
+  });
+});
