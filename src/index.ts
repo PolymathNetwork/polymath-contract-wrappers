c85dacc1fbba3f1930ae1b5828f867dc7deb8467
Jeremias Diaz <monitz87@gmail.com>
2019-07-08 13:21:37 -0400

    feat: export all module classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { default as CappedSTO } from './contract_wrappers/modules/sto/capped_sto_wrapper';
 export { default as ContractWrapper } from './contract_wrappers/contract_wrapper';
 export { default as FeatureRegistry } from './contract_wrappers/registries/feature_registry_wrapper';
 export { default as ModuleFactory } from './contract_wrappers/modules/module_factory_wrapper';
@@ -7,11 +6,33 @@ export { default as PolyToken } from './contract_wrappers/tokens/poly_token_wrap
 export { default as PolymathRegistry } from './contract_wrappers/registries/polymath_registry_wrapper';
 export { default as SecurityTokenRegistry } from './contract_wrappers/registries/security_token_registry_wrapper';
 export { default as SecurityToken } from './contract_wrappers/tokens/security_token_wrapper';
-export { default as USDTieredSTO } from './contract_wrappers/modules/sto/usd_tiered_sto_wrapper';
 export { default as ERC20 } from './contract_wrappers/tokens/erc20_wrapper';
 export {
   default as ERC20DividendCheckpoint,
 } from './contract_wrappers/modules/checkpoint/erc20_dividend_checkpoint_wrapper';
+export {
+  default as EtherDividendCheckpoint,
+} from './contract_wrappers/modules/checkpoint/ether_dividend_checkpoint_wrapper';
+export {
+  default as GeneralPermissionManager,
+} from './contract_wrappers/modules/permission_manager/general_permission_manager_wrapper';
+export { default as CappedSTO } from './contract_wrappers/modules/sto/capped_sto_wrapper';
+export { default as USDTieredSTO } from './contract_wrappers/modules/sto/usd_tiered_sto_wrapper';
+export {
+  default as CountTransferManager,
+} from './contract_wrappers/modules/transfer_manager/count_transfer_manager_wrapper';
+export {
+  default as GeneralTransferManager,
+} from './contract_wrappers/modules/transfer_manager/general_transfer_manager_wrapper';
+export {
+  default as ManualApprovalTransferManager,
+} from './contract_wrappers/modules/transfer_manager/manual_approval_transfer_manager_wrapper';
+export {
+  default as PercentageTransferManager,
+} from './contract_wrappers/modules/transfer_manager/percentage_transfer_manager_wrapper';
+export {
+  default as VolumeRestrictionTransferManager,
+} from './contract_wrappers/modules/transfer_manager/volume_restriction_transfer_manager_wrapper';
 export * from './types';
 export { PolymathAPI } from './PolymathAPI';
 export { BlockParamLiteral } from 'ethereum-types';
