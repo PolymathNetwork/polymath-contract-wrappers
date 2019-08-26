9426f283c1f4ade80efcac6cc91ef8ccb1a4276d
Victor Wiebe <victor.wiebe@polymath.network>
2019-08-26 08:19:39 -0800

    feat: 🎸 fix comments in sto wrapper, fix some return types/test

--- src/contract_wrappers/modules/sto/sto_wrapper.ts
+++ src/contract_wrappers/modules/sto/sto_wrapper.ts
@@ -1,8 +1,12 @@
+import { BigNumber } from '@polymathnetwork/abi-wrappers';
 import { TxParams, STOBaseContract, FundRaiseType, FULL_DECIMALS } from '../../../types';
 import ModuleWrapper from '../module_wrapper';
 import assert from '../../../utils/assert';
-import { weiToValue } from '../../../utils/convert';
+import { bigNumberToDate, weiToValue } from '../../../utils/convert';
 
+/**
+ * @param type The FundRaiseType
+ */
 interface FundRaiseTypesParams {
   type: FundRaiseType;
 }
@@ -13,80 +17,107 @@ interface FundRaiseTypesParams {
 export default abstract class STOWrapper extends ModuleWrapper {
   protected abstract contract: Promise<STOBaseContract>;
 
-  public paused = async () => {
+  /**
+   *  check if the module is paused
+   *  @return boolean status of paused
+   */
+  public paused = async (): Promise<boolean> => {
     return (await this.contract).paused.callAsync();
   };
 
-  public securityToken = async () => {
+  /**
+   *  security token address
+   *  @return address
+   */
+  public securityToken = async (): Promise<string> => {
     return (await this.contract).securityToken.callAsync();
   };
 
   /**
    * Type of currency used to collect the funds
+   * @return boolean corresponding to fund raise type
    */
-  public fundRaiseTypes = async (params: FundRaiseTypesParams) => {
+  public fundRaiseTypes = async (params: FundRaiseTypesParams): Promise<boolean> => {
     return (await this.contract).fundRaiseTypes.callAsync(params.type);
   };
 
   /**
    * Returns funds raised by the STO
+   * @return amount of funds raised
    */
-  public fundsRaised = async (params: FundRaiseTypesParams) => {
+  public fundsRaised = async (params: FundRaiseTypesParams): Promise<BigNumber> => {
     return weiToValue(await (await this.contract).fundsRaised.callAsync(params.type), FULL_DECIMALS);
   };
 
   /**
-   * Start time of the Capped STO
+   * Start time of the STO
+   * @returns startTime
    */
-  public startTime = async () => {
-    return (await this.contract).startTime.callAsync();
+  public startTime = async (): Promise<Date> => {
+    return bigNumberToDate(await (await this.contract).startTime.callAsync());
   };
 
   /**
-   * End time of the Capped STO
+   * End time of the STO
+   * @returns endTime
    */
-  public endTime = async () => {
-    return (await this.contract).endTime.callAsync();
+  public endTime = async (): Promise<Date> => {
+    return bigNumberToDate(await (await this.contract).endTime.callAsync());
   };
-
-  public pausedTime = async () => {
-    return (await this.contract).pausedTime.callAsync();
+  
+  /**
+   * End time of the Capped STO
+   * @returns pausedTime
+   */
+  public pausedTime = async (): Promise<Date> => {
+    return bigNumberToDate(await (await this.contract).pausedTime.callAsync());
   };
 
   /**
    * Number of individual investors this STO have.
+   * @return number of investors
    */
-  public investorCount = async () => {
-    return (await this.contract).investorCount.callAsync();
+  public investorCount = async (): Promise<number> => {
+    return (await (await this.contract).investorCount.callAsync()).toNumber();
   };
 
   /**
    * Ethereum account address to hold the funds
+   * @return wallet address
    */
-  public wallet = async () => {
+  public wallet = async (): Promise<string> => {
     return (await this.contract).wallet.callAsync();
   };
 
   /**
    * Return the total no. of tokens sold
    */
-  public totalTokensSold = async () => {
+  public totalTokensSold = async (): Promise<BigNumber> => {
     return weiToValue(
       await (await this.contract).totalTokensSold.callAsync(),
       await (await this.securityTokenContract()).decimals.callAsync(),
     );
   };
 
-  public getRaised = async (params: FundRaiseTypesParams) => {
+  /**
+   * Returns funds raised by the STO
+   */
+  public getRaised = async (params: FundRaiseTypesParams): Promise<BigNumber> => {
     return weiToValue(await (await this.contract).getRaised.callAsync(params.type), FULL_DECIMALS);
   };
 
+  /**
+   *  pause the module
+   */
   public pause = async (params: TxParams) => {
     assert.assert(!(await this.paused()), 'Contract already paused');
     assert.assert(await this.isCallerTheSecurityTokenOwner(params.txData), 'The caller must be the ST owner');
     return (await this.contract).pause.sendTransactionAsync(params.txData, params.safetyFactor);
   };
 
+  /**
+   *  unpause the module
+   */
   public unpause = async (params: TxParams) => {
     assert.assert(await this.paused(), 'Contract is not paused');
     assert.assert(await this.isCallerTheSecurityTokenOwner(params.txData), 'The caller must be the ST owner');
