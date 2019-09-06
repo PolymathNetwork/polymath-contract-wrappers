4b6e660ec41c8b3d72dd3130b280ecd99165159a
Shuffledex <shuffledex@gmail.com>
2019-09-06 09:19:57 -0300

    fix: 🐛 add bundle-analyzer to visualize size of project

--- webpack.base.js
+++ webpack.base.js
@@ -1,30 +1,32 @@
 /* Base configuration shared accross webpack bundles */
-const path = require("path");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-  devtool: "cheap-module-source-map",
-  mode: "development",
+  devtool: 'cheap-module-source-map',
+  mode: 'development',
   module: {
     rules: [
       // Loader that compiles Typescript files into standard Javascript
       {
         test: /\.ts$/,
-        loader: "awesome-typescript-loader"
-      }
-    ]
+        loader: 'awesome-typescript-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
     plugins: [
       // Reads the "paths" field in tsconfig to generate import aliases
       // This can be used for enabling root imports for example.
-      new TsconfigPathsPlugin()
-    ]
+      new TsconfigPathsPlugin(),
+    ],
   },
   plugins: [
     // Ensures import paths match case of actual files in disk
-    new CaseSensitivePathsPlugin()
-  ]
+    new CaseSensitivePathsPlugin(),
+    new BundleAnalyzerPlugin(),
+  ],
 };
