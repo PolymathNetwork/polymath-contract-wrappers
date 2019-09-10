8c3abc22ecf08fe3f82fec1d3a6250733fdbd45f
Victor Wiebe <victor.wiebe@polymath.network>
2019-09-10 05:54:37 -0800

    refactor: 💡 addInvestorToWhitelist with exported launch methods

--- examples/addInvestorToWhitelist.ts
+++ examples/addInvestorToWhitelist.ts
@@ -2,6 +2,8 @@ import { RedundantSubprovider, RPCSubprovider, Web3ProviderEngine } from '@0x/su
 import { GeneralTransferManagerEvents, BigNumber } from '@polymathnetwork/abi-wrappers';
 import { ApiConstructorParams, PolymathAPI } from '../src/PolymathAPI';
 import { ModuleName } from '../src';
+import {registerTicker} from './example_components/registerTickerComponent';
+import {launchToken} from './example_components/launchTokenComponent';
 
 // This file acts as a valid sandbox.ts file in root directory for adding a new address to the whitelist on an unlocked node (like ganache)
 
@@ -26,40 +28,10 @@ window.addEventListener('load', async () => {
   const ticker = prompt('Ticker', '');
   const tokenName = prompt('Token Name', '');
 
-  // Double check available
-  await polymathAPI.securityTokenRegistry.tickerAvailable({
-    ticker: ticker!,
-  });
-
-  // Get the ticker fee and approve the security token registry to spend
-  const tickerFee = await polymathAPI.securityTokenRegistry.getTickerRegistrationFee();
-  await polymathAPI.polyToken.approve({
-    spender: await polymathAPI.securityTokenRegistry.address(),
-    value: tickerFee,
-  });
-
-  // Register a ticker
-  await polymathAPI.securityTokenRegistry.registerTicker({
-    ticker: ticker!,
-    tokenName: tokenName!,
-  });
-
-  // Get the st launch fee and approve the security token registry to spend
-  const securityTokenLaunchFee = await polymathAPI.securityTokenRegistry.getSecurityTokenLaunchFee();
-  await polymathAPI.polyToken.approve({
-    spender: await polymathAPI.securityTokenRegistry.address(),
-    value: securityTokenLaunchFee,
-  });
-
-  // Generate a security token
-  await polymathAPI.securityTokenRegistry.generateNewSecurityToken({
-    name: tokenName!,
-    ticker: ticker!,
-    tokenDetails: 'http://',
-    divisible: false,
-    treasuryWallet: myAddress,
-    protocolVersion: '0',
-  });
+  // Register ticker and launch token
+  await registerTicker(polymathAPI, ticker ? ticker : '', myAddress);
+  await launchToken(polymathAPI, tokenName ? tokenName : '', ticker ? ticker : '', 'http://', myAddress, false);
+  console.log('Ticker was registered and token was launched successfully!');
 
   // Create a Security Token Instance
   const tickerSecurityTokenInstance = await polymathAPI.tokenFactory.getSecurityTokenInstanceFromTicker(ticker!);
