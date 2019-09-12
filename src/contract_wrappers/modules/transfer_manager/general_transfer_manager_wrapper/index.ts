57c116a12123c7959247f218efe6c4f6164ef8e4
Victor Wiebe <victor.wiebe@polymath.network>
2019-09-12 09:09:51 -0800

    feat: 🎸 add new versioned architecture wrappers GTM 3.0 and 3.1

---
+++ src/contract_wrappers/modules/transfer_manager/general_transfer_manager_wrapper/index.ts
@@ -0,0 +1,13 @@
+/* istanbul ignore file */
+import { GeneralTransferManager_3_0_0, isGeneralTransferManager_3_0_0 } from './3.0.0';
+import { GeneralTransferManager_3_1_0, isGeneralTransferManager_3_1_0 } from './3.1.0';
+
+export type CountTransferManager = GeneralTransferManager_3_0_0 | GeneralTransferManager_3_1_0;
+
+export {
+  GeneralTransferManager_3_0_0,
+  isGeneralTransferManager_3_0_0,
+  GeneralTransferManager_3_1_0,
+  isGeneralTransferManager_3_1_0,
+};
+export { default as GeneralTransferManagerCommon } from './common';
