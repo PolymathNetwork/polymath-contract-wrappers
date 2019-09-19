0c6c3013e696460ba761d878f587424f81cb11b5
Shuffledex <shuffledex@gmail.com>
2019-09-19 18:40:46 -0300

    fix: 🐛 add isLockUpTransferManager_3_0_0 method

--- src/contract_wrappers/modules/transfer_manager/lock_up_transfer_manager_wrapper/3.0.0.ts
+++ src/contract_wrappers/modules/transfer_manager/lock_up_transfer_manager_wrapper/3.0.0.ts
@@ -1,3 +1,10 @@
 import LockUpTransferManagerCommon from './common';
+import { ContractVersion } from '../../../../types';
 
 export class LockUpTransferManager_3_0_0 extends LockUpTransferManagerCommon {}
+
+export function isLockUpTransferManager_3_0_0(
+  wrapper: LockUpTransferManagerCommon,
+): wrapper is LockUpTransferManager_3_0_0 {
+  return wrapper.contractVersion === ContractVersion.V3_0_0;
+}
