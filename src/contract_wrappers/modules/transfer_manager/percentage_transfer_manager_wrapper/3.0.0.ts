ca464603a31de3c174a82289c7928309ffc3cab3
Shuffledex <shuffledex@gmail.com>
2019-09-20 13:04:44 -0300

    fix: 🐛 remove construct method

--- src/contract_wrappers/modules/transfer_manager/percentage_transfer_manager_wrapper/3.0.0.ts
+++ src/contract_wrappers/modules/transfer_manager/percentage_transfer_manager_wrapper/3.0.0.ts
@@ -1,29 +1,8 @@
-import { PercentageTransferManagerContract_3_0_0, Web3Wrapper } from '@polymathnetwork/abi-wrappers';
 import { ContractVersion } from '../../../../types';
-import ContractFactory from '../../../../factories/contractFactory';
 
 import PercentageTransferManagerCommon from './common';
 
-export class PercentageTransferManager_3_0_0 extends PercentageTransferManagerCommon {
-  public contract: Promise<PercentageTransferManagerContract_3_0_0>;
-
-  public contractVersion = ContractVersion.V3_0_0;
-
-  /**
-   * Instantiate PercentageTransferManager_3_0_0
-   * @param web3Wrapper Web3Wrapper instance to use
-   * @param contract
-   * @param contractFactory
-   */
-  public constructor(
-    web3Wrapper: Web3Wrapper,
-    contract: Promise<PercentageTransferManagerContract_3_0_0>,
-    contractFactory: ContractFactory,
-  ) {
-    super(web3Wrapper, contract, contractFactory);
-    this.contract = contract;
-  }
-}
+export class PercentageTransferManager_3_0_0 extends PercentageTransferManagerCommon {}
 
 export function isPercentageTransferManager_3_0_0(
   wrapper: PercentageTransferManagerCommon,
