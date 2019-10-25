068e0bcde9772e51192113986f8f55bc2d2cd54d
Jeremias Diaz <monitz87@gmail.com>
2019-10-25 00:07:41 -0300

    fix: 🐛 fix faulty non-hex default

--- src/contract_wrappers/tokens/security_token_wrapper/common.ts
+++ src/contract_wrappers/tokens/security_token_wrapper/common.ts
@@ -2056,7 +2056,7 @@ export default abstract class SecurityTokenCommon extends ERC20TokenWrapper {
       producedAddModuleInfo.data,
       producedAddModuleInfo.maxCost,
       producedAddModuleInfo.budget,
-      params.label ? stringToBytes32(params.label) : '',
+      stringToBytes32(params.label || ''),
       params.archived,
       params.txData,
       params.safetyFactor,
